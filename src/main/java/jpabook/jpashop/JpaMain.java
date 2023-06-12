b78ef77624f2d287e7283a6a438fc35fa093bafd
jsbyeon <jsbyeon@enliple.com>
2023-06-12 13:56:17 +0900

    jpashop 기본 파일 세팅, db 설정

---
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,30 @@
+package jpabook.jpashop;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        // 최초 1회만 실행 해야함
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        // 트랜잭션 별 생성 계속 해야 함
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,55 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "MEMBER_ID")
+    private Long id;
+    private String name;
+    private String ciry;
+    private String street;
+    private String zipcode;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCiry() {
+        return ciry;
+    }
+
+    public void setCiry(String ciry) {
+        this.ciry = ciry;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+}
